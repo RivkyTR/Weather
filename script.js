7f7c5f3bbda9f9064a112a3468e42dd4dd921e28
RivkyTR <Rivka4555@gmail.com>
2024-09-17 17:23:19 +0300

    adding the icon

--- script.js
+++ script.js
@@ -9,18 +9,27 @@ const apiKey = '8ee633956bad6ae1965b557a94ecfcba';
 async function setWeather(name, cityId){
     const url = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&lang=he&units=metric`;
     try {
-            const response = await fetch(url);
-            const data = await response.json();
+        const response = await fetch(url);
+        const data = await response.json();
         
-            // Updating the cards with the weather data
-            document.getElementById(`temp-${name}`).textContent = `${Math.round(data.main.temp)}°C`;
-            document.getElementById(`feels-like-${name}`).textContent = `${Math.round(data.main.feels_like)}°C`;
-            document.getElementById(`humidity-${name}`).textContent = `${data.main.humidity}%`;
-            document.getElementById(`description-${name}`).textContent = data.weather[0].description;
-        
-            
+        // Updating the cards with the weather data
+        document.getElementById(`temp-${name}`).textContent = `${Math.round(data.main.temp)}°C`;
+        document.getElementById(`feels-like-${name}`).textContent = `${Math.round(data.main.feels_like)}°C`;
+        document.getElementById(`humidity-${name}`).textContent = `${data.main.humidity}%`;
+        document.getElementById(`description-${name}`).textContent = data.weather[0].description;
+
+        // set the icon based on the temperature
+        const temp = data.main.temp;
+        const iconElement = document.getElementById(`icon-${name}`);
+        if (temp > 30) {
+            iconElement.src = 'https://img.icons8.com/emoji/48/sun-emoji.png';
+        } else if(temp>20){
+            iconElement.src = 'https://img.icons8.com/emoji/48/sun-behind-cloud.png';
+        } else{
+            iconElement.src = 'https://img.icons8.com/emoji/48/snowflake-emoji.png';
+        }
     } catch (error) {
-            console.error('Error fetching weather data:', error);
+        console.error('Error fetching weather data with API call:', error);
     }
 }
 
@@ -31,4 +40,7 @@ function setWeatherForAll(){
 
 window.onload = () => {
     setWeatherForAll();
+    
+    //every 15 minutes refresh the weather data
+    setInterval(setWeatherForAll, 900000);
   };    
